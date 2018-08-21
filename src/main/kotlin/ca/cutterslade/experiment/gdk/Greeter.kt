5c3a15000f2ecda7b6563e008c9f45acac373054
Wesley Hartford <wesley@ze.com>
2018-08-21 09:57:04 -0700

    Add @Inject annotated constructor

--- src/main/kotlin/ca/cutterslade/experiment/gdk/Greeter.kt
+++ src/main/kotlin/ca/cutterslade/experiment/gdk/Greeter.kt
@@ -2,9 +2,10 @@ package ca.cutterslade.experiment.gdk
 
 import dagger.grpc.server.GrpcService
 import io.grpc.stub.StreamObserver
+import javax.inject.Inject
 
 @GrpcService(grpcClass = GreeterGrpc::class)
-class Greeter : GreeterGrpc.GreeterImplBase() {
+class Greeter @Inject constructor() : GreeterGrpc.GreeterImplBase() {
   override fun sayHello(request: HelloRequest, responseObserver: StreamObserver<HelloReply>) {
     val reply = HelloReply.newBuilder().setMessage("Hello ${request.name}").build()
     responseObserver.onNext(reply)
