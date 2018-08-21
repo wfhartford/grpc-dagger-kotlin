10ef3c740f91d4c3a1ee37a33d567a2251ae9d11
Wesley Hartford <wesley@ze.com>
2018-08-21 09:56:19 -0700

    Make the module concrete

--- src/main/kotlin/ca/cutterslade/experiment/gdk/GreeterModule.kt
+++ src/main/kotlin/ca/cutterslade/experiment/gdk/GreeterModule.kt
@@ -1,20 +1,16 @@
 package ca.cutterslade.experiment.gdk
 
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.grpc.server.ForGrpcService
 import io.grpc.ServerInterceptor
 
 @Module
-abstract class GreeterModule {
-  @Binds
-  abstract fun greeterComponent(component: GreeterComponent): GreeterServiceDefinition
+class GreeterModule {
+  @Provides
+  fun greeterComponent(component: GreeterComponent): GreeterServiceDefinition = component
 
-  companion object {
-    @Provides
-    @ForGrpcService(GreeterGrpc::class)
-    @JvmStatic
-    fun interceptors(): List<ServerInterceptor> = listOf()
-  }
+  @Provides
+  @ForGrpcService(GreeterGrpc::class)
+  fun interceptors(): List<ServerInterceptor> = listOf()
 }
